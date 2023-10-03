f4c034916526e7a33013b92814698ec0a83a543c
Titah Baudwin <baudwin12@gmail.com>
2023-10-04 00:34:40 +0100

    latest commit

--- info.js
+++ info.js
@@ -102,3 +102,54 @@
 
 // /api/product/comment/:productId  :: POST
 // /api/product/contact/:productId  :: POST
+
+
+
+
+// CREATE DATABASE ClassifiedApp;
+
+
+// CREATE TABLE User(
+// id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT
+// username varchar(50) NOT NULL UNIQUE,
+// email varchar(50) NOT NULL UNIQUE,
+// passwd varchar(50) NOT NULL,
+// created_on datetime,
+// updated_on datetime
+// );
+
+
+// CREATE TABLE Category(
+// id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+// title varchar(50) NOT NULL,
+// created_on datetime,
+// updated_on datetime
+// );
+
+// CREATE TABLE Products(
+// id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT
+// category_id int NOT NULL,
+// title varchar(50) NOT NULL UNIQUE,
+// price varchar(50) NOT NULL UNIQUE,
+// paddress varchar(50) NOT NULL,
+// pstatus varchar(50) NOT NULL default=1,
+// created_on datetime,
+// updated_on datetime,
+// FOREIGN KEY(category_id) REFERENCES Category(id)
+
+// );
+
+
+// CREATE TABLE Contact(
+// id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT
+// userid int NOT NULL,
+// productid int NOT NULL,
+// cmessage varchar(250) NOT NULL UNIQUE,
+// created_on datetime,
+// updated_on datetime,
+// FOREIGN KEY(userid) REFERENCES User(id)
+// ,
+// FOREIGN KEY(productid) REFERENCES Products(id)
+
+// );
+
