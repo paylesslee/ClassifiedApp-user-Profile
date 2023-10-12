4bf1a706e260f5c5731fa7ba8e08c9f7458d6138
Yanbeckford <yanbeckford@gmail.com>
2023-10-12 02:20:43 +0100

    commit nber 2

--- src/routes/user.js
+++ src/routes/user.js
@@ -116,6 +116,7 @@ router.put("/update/:id", async (request, response) => {
   }
 });   
 
+module.exports=router;
 
 
 
@@ -171,7 +172,6 @@ router.put("/update/:id", async (request, response) => {
 
 
 
-module.exports=router;
 
 
 
