0972fb21fe369839af4978fc0af65fd8192dd588
Yanbeckford <yanbeckford@gmail.com>
2023-10-12 02:39:42 +0100

    Team B commit 2

---
+++ src/routes/comments.js
@@ -0,0 +1,10 @@
+const {Router} = require('express')
+const router = Router() 
+const db = require("../config/db")
+
+router.post('/comment/:productID ', (req, res) => {
+    
+});
+
+
+module.exports=router
---
+++ src/routes/contact.js
@@ -0,0 +1,11 @@
+const {Router} = require('express')
+const router = Router() 
+const db = require("../config/db")
+
+
+router.post('/contact/:productID ', (req, res) => {
+    
+});
+
+
+module.exports=router
