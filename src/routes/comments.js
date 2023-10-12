6cd559838acf2ed6776ea7f259c3d2bb62c0bf74
Yanbeckford <yanbeckford@gmail.com>
2023-10-12 03:24:30 +0100

    Team B Final commit

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -2,9 +2,37 @@ const {Router} = require('express')
 const router = Router() 
 const db = require("../config/db")
 
-router.post('/comment/:productID ', (req, res) => {
+function queryPromise(sql,values=[]){
+    return new Promise((resolve,reject)=>{
+      db.query(sql,values,(error,result)=>{
+        if(error){
+          reject(error)
+        }else{
+          resolve(result)
+        }
+      })
+    });
+  }  
+
+  // Commenting a particular product
+router.post('/comment/:productID ', async(request, response) => {
+    try{
+        const { userid, productid, cmessage} = request.body;
+        if(!userid || !productid || !cmessage){
+            respond.send("Enter values")
+        } 
+        const uservalues = [userid, productid, cmessage];
+        const myquery = "INSERT INTO Comment(userid, productid, cmessage, created_on ) VALUES (?,?,?,now())"
+        const result = await queryPromise(myquery,uservalues)
+        respond.send("Comment sent")
+  
+  
+    }catch(err){
+        console.log(err)
+    }
     
-});
+    });
+
 
 
 module.exports=router
--- src/routes/contact.js
+++ src/routes/contact.js
@@ -2,10 +2,36 @@ const {Router} = require('express')
 const router = Router() 
 const db = require("../config/db")
 
+function queryPromise(sql,values=[]){
+    return new Promise((resolve,reject)=>{
+      db.query(sql,values,(error,result)=>{
+        if(error){
+          reject(error)
+        }else{
+          resolve(result)
+        }
+      })
+    });
+  }  
 
-router.post('/contact/:productID ', (req, res) => {
+// contacting for a particular product
+router.post('/contact/:productID ', async(req, res) => {
+    try{
+        const { userid, productid, cmessage} = request.body;
+        if(!userid || !productid || !cmessage){
+            respond.send("Enter values")
+        } 
+        const uservalues = [userid, productid, cmessage];
+        const myquery = "INSERT INTO Comment(userid, productid, cmessage, created_on ) VALUES (?,?,?,now())"
+        const result = await queryPromise(myquery,uservalues)
+        respond.send("Contact Message sent")
+  
+  
+    }catch(err){
+        console.log(err)
+    }
     
-});
+    });
 
 
 module.exports=router
