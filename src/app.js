6a8d4b003e1e8e19fca7999bf5ac2f28c2554e8e
Yanbeckford <yanbeckford@gmail.com>
2023-11-06 18:54:13 +0100

    actions implementation

--- src/app.js
+++ src/app.js
@@ -31,9 +31,14 @@ app.use("/api/comment",comments)
 app.use("/api/contact",contact)
 
 
-app.get('/welcome', (request, response)=>{
-   response.render('pages/index', {})
-
+app.get('/user', (request, response)=>{
+   db.query("SELECT * FROM User", function(err,result){
+      if(err){
+         console.log(err)
+      }else{
+         response.render('pages/index',{blog_post:result})
+      }
+     })
 })
 
 app.get('/login', (request, response)=>{
@@ -60,18 +65,77 @@ app.get('/create_category', (request, response)=>{
    response.render('pages/create_category', {})
 })
 
-app.get('/create_personalInfo', (request, response)=>{
-   response.render('pages/create personal_info', {})
+app.post('/category_new', (request, response)=>{
+   try{
+      const {title,created_on} = request.body;
+      if(!title && !created_on){
+        response.render('pages/index')
+      }
+      const titlevalue = [title];
+      const myquery = "INSERT INTO Category (title,created_on) VALUES(?,now())";
+      db.query(myquery,titlevalue, function(error,results){
+         if(error){
+            console.log(error)
+         }else{
+         response.redirect('/user')
+         }
+         })
+   }catch(error){
+            console.log(error)
+         }
+      })
+
+app.get('/update_personal_info', (request, response)=>{
+   response.render('pages/update_personal_info', {})
 })
 
-app.get('/update_personalInfo', (request, response)=>{
-   response.render('pages/update personal_info', {})
+app.put('/update_info', (request, response)=>{
+   try{
+      const {userid,username,email,phonenumber, passwd, created_on} = request.body;
+      if(!userid && !username && !email && !phonenumber && passwd){
+        response.render('pages/create_personal_info')
+      }
+      const titlevalue = [userid,username,email,phonenumber, passwd, created_on];
+      const myquery = "INSERT INTO User(userid,username,email,phonenumber, passwd, created_on) VALUES(?,?,?,?,?,now())";
+      db.query(myquery,titlevalue, function(error,results){
+         if(error){
+            console.log(error)
+         }else{
+         response.redirect('/user')
+         }
+         })
+   }catch(error){
+            console.log(error)
+         }
+      })
+
+app.get('/create_personal_info', (request, response)=>{
+   response.render('pages/create_personal_info', {})
 })
 
 app.get('/create_product', (request, response)=>{
    response.render('pages/create_product', {})
 })
 
+app.post('/product_new', (request, response)=>{
+   try{
+      const {userid,categoryid,title,price, paddress, created_on} = request.body;
+      if(!categoryid && !title && !price && !paddress){
+        response.render('pages/index')
+      }
+      const titlevalue = [userid,categoryid,title,price, paddress, created_on];
+      const myquery = "INSERT INTO Products(userid,categoryid,title,price, paddress, created_on) VALUES(?,?,?,?,?,now())";
+      db.query(myquery,titlevalue, function(error,results){
+         if(error){
+            console.log(error)
+         }else{
+         response.redirect('/user')
+         }
+         })
+   }catch(error){
+            console.log(error)
+         }
+      })
 
   app.listen(port, () => { 
   console.log(`Server started on port ${port}` );
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host     : 'localhost', // default
   user     : 'root', // default
-  password : 'i23456@789', //database password
+  password : 'Y@n123456', //database password
   database : 'ClassifiedApp' // database name
 })
  
